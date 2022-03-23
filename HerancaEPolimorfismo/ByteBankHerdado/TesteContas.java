da724adfa5ef6334e74320fc019f7d746c3b8eaa
IgorSMendes <igor07082001@gmail.com>
2022-03-23 16:35:41 -0300

    Modificando o arquivo

--- HerancaEPolimorfismo/ByteBankHerdado/TesteContas.java
+++ HerancaEPolimorfismo/ByteBankHerdado/TesteContas.java
@@ -6,5 +6,8 @@ public class TesteContas {
         cc.deposita(222.00);
         ContaPoupanca cp=new ContaPoupanca(211, 2345);
         cp.deposita(2465.00);
+        cp.transfere(cc, 1200.00);
+        System.out.println(cc.getSaldo());   
+        
     }
 }
