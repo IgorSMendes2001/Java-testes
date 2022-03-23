ca1a95d5110ea2b8ddb17d7579ad050a50f2f3d0
IgorSMendes <igor07082001@gmail.com>
2022-03-23 16:51:34 -0300

    Atualizando

--- HerancaEPolimorfismo/ByteBankHerdado/ContaCorrente.java
+++ HerancaEPolimorfismo/ByteBankHerdado/ContaCorrente.java
@@ -4,4 +4,9 @@ public class ContaCorrente extends Conta{//Herda atributos e métodos mas nunca 
     public ContaCorrente(int agencia,int numero){
        super(agencia,numero);
     }
+    @Override
+    public boolean saca(double valor) {
+        // TODO Auto-generated method stub
+        return super.saca(valor);
+    }
 }
