ce70a977e195a3e21f5dfcda05e457efdcacab6a
IgorSMendes <igor07082001@gmail.com>
2022-04-05 13:19:01 -0300

    New Changes

---
+++ JavaArrayList/TestandoCollections/src/TestaListaDeAula.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class TestaListaDeAula {
+    public static void main(String[] args) {
+        Aula a1=new Aula("Revisitando os ArrayLists",21);
+        Aula a2=new Aula("Listas de objetos", 15);
+        Aula a3=new Aula("Relacionamento de listas e objetos", 15);
+
+        ArrayList<Aula>aulas=new ArrayList<>();
+        aulas.add(a1);
+        aulas.add(a2);
+        aulas.add(a3);
+        System.out.println(aulas);
+        Collections.sort(aulas, Comparator.comparing(Aula::getTempo));
+        System.out.println(aulas);
+        aulas.sort(Comparator.comparing(Aula::getTempo));
+        System.out.println(aulas);
+    }
+}
